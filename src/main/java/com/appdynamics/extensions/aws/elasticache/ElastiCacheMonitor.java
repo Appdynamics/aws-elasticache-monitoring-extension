9b88575f7558a46069f377470961c257ebc584b3
akshayAppd <akshay.srivastava@appdynamics.com>
2018-07-13 12:23:10 +0530

    Revamp to AWS 2.0.0

--- src/main/java/com/appdynamics/extensions/aws/elasticache/ElastiCacheMonitor.java
+++ src/main/java/com/appdynamics/extensions/aws/elasticache/ElastiCacheMonitor.java
@@ -12,15 +12,25 @@ import static com.appdynamics.extensions.aws.Constants.METRIC_PATH_SEPARATOR;
 
 import com.appdynamics.extensions.aws.SingleNamespaceCloudwatchMonitor;
 import com.appdynamics.extensions.aws.collectors.NamespaceMetricStatisticsCollector;
-import com.appdynamics.extensions.aws.elasticache.config.ElastiCacheConfiguration;
+import com.appdynamics.extensions.aws.config.Configuration;
 import com.appdynamics.extensions.aws.metric.processors.MetricsProcessor;
-import org.apache.commons.lang.StringUtils;
+import com.singularity.ee.agent.systemagent.api.exception.TaskExecutionException;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Florencio Sarmiento
  */
-public class ElastiCacheMonitor extends SingleNamespaceCloudwatchMonitor<ElastiCacheConfiguration> {
+public class ElastiCacheMonitor extends SingleNamespaceCloudwatchMonitor<Configuration> {
 
     private static final Logger LOGGER = Logger.getLogger("com.singularity.extensions.aws.ElastiCacheMonitor");
 
@@ -28,22 +38,39 @@ public class ElastiCacheMonitor extends SingleNamespaceCloudwatchMonitor<ElastiC
             "Custom Metrics", METRIC_PATH_SEPARATOR, "Amazon ElastiCache", METRIC_PATH_SEPARATOR);
 
     public ElastiCacheMonitor() {
-        super(ElastiCacheConfiguration.class);
+        super(Configuration.class);
         LOGGER.info(String.format("Using AWS ElastiCacheMonitor Monitor Version [%s]",
                 this.getClass().getPackage().getImplementationTitle()));
     }
 
     @Override
+    public String getDefaultMetricPrefix() {
+        return DEFAULT_METRIC_PREFIX;
+    }
+
+    @Override
+    public String getMonitorName() {
+        return "ElastiCacheMonitor";
+    }
+
+    @Override
+    protected int getTaskCount() {
+        return 3;
+    }
+
+
+    @Override
     protected NamespaceMetricStatisticsCollector getNamespaceMetricsCollector(
-            ElastiCacheConfiguration config) {
+            Configuration config) {
         MetricsProcessor metricsProcessor = createMetricsProcessor(config);
 
         return new NamespaceMetricStatisticsCollector
                 .Builder(config.getAccounts(),
                 config.getConcurrencyConfig(),
                 config.getMetricsConfig(),
-                metricsProcessor)
-                .withCredentialsEncryptionConfig(config.getCredentialsDecryptionConfig())
+                metricsProcessor,
+                config.getMetricPrefix())
+                .withCredentialsDecryptionConfig(config.getCredentialsDecryptionConfig())
                 .withProxyConfig(config.getProxyConfig())
                 .build();
     }
@@ -53,16 +80,38 @@ public class ElastiCacheMonitor extends SingleNamespaceCloudwatchMonitor<ElastiC
         return LOGGER;
     }
 
-    @Override
-    protected String getMetricPrefix(ElastiCacheConfiguration config) {
-        return StringUtils.isNotBlank(config.getMetricPrefix()) ?
-                config.getMetricPrefix() : DEFAULT_METRIC_PREFIX;
+    private MetricsProcessor createMetricsProcessor(Configuration config) {
+        return new ElastiCacheMetricsProcessor(
+                config.getMetricsConfig().getIncludeMetrics(),
+                config.getDimensions());
     }
 
-    private MetricsProcessor createMetricsProcessor(ElastiCacheConfiguration config) {
-        return new ElastiCacheMetricsProcessor(
-                config.getMetricsConfig().getMetricTypes(),
-                config.getMetricsConfig().getExcludeMetrics(),
-                config.getIncludeCacheClusters());
+    public static void main(String[] args) throws TaskExecutionException {
+
+        ConsoleAppender ca = new ConsoleAppender();
+        ca.setWriter(new OutputStreamWriter(System.out));
+        ca.setLayout(new PatternLayout("%-5p [%t]: %m%n"));
+        ca.setThreshold(Level.DEBUG);
+
+        Logger.getRootLogger().addAppender(ca);
+
+        final ElastiCacheMonitor monitor = new ElastiCacheMonitor();
+
+        final Map<String, String> taskArgs = new HashMap<String, String>();
+        taskArgs.put("config-file", "/Users/akshay.srivastava/AppDynamics/extensions/aws-elasticache-monitoring-extension/src/main/resources/config.yml");
+        //taskArgs.put("metric-file", "/Users/akshay.srivastava/AppDynamics/extensions/apache-monitoring-extension/src/main/resources/conf/metrics.xml");
+
+        //monitor.execute(taskArgs, null);
+
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(new Runnable() {
+            public void run() {
+                try {
+                    monitor.execute(taskArgs, null);
+                } catch (Exception e) {
+                    System.out.println("Error while running the task" + e);
+                }
+            }
+        }, 2, 30, TimeUnit.SECONDS);
     }
 }
